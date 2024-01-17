09e8372d5ee218da9c98099716ab6dda10588066
SARANKUMARTSK <sarankumartsk@gmail.com>
2024-01-17 13:54:16 +0530

    Edited

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
   <BrowserRouter>
   <Sidebar/>
   <Topbar />
-   <AddProduct/>
     <Routes>
       <Route path='/dashboard' element={<Dashboard />}/>
       <Route path='/admin' element={<Admin/>}/>
